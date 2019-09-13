e87997843712b7ebb8023893a3be2ff4c1e78ac0
trentmoss05 <trentmoss05@gmail.com>
2019-09-13 17:50:50 -0400

    Updates

---
+++ precache-manifest.b6607933fd72e4f4e78989a5d2f27a27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1764a79bcd54ed66a092",
+    "url": "/react-lights-out/static/js/runtime~main.1764a79b.js"
+  },
+  {
+    "revision": "0aa38e7fd6c6fa953876",
+    "url": "/react-lights-out/static/js/main.0aa38e7f.chunk.js"
+  },
+  {
+    "revision": "03109274422a2c9f0101",
+    "url": "/react-lights-out/static/js/1.03109274.chunk.js"
+  },
+  {
+    "revision": "0aa38e7fd6c6fa953876",
+    "url": "/react-lights-out/static/css/main.b682aa51.chunk.css"
+  },
+  {
+    "revision": "9f054454453a742078ccb5ad3fd03878",
+    "url": "/react-lights-out/index.html"
+  }
+];
